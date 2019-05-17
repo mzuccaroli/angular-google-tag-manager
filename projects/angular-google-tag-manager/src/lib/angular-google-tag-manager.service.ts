fc65fd9761b5a549e1733130cb209133f3d09977
mzuccaroli <marcozuccaroli@gmail.com>
2019-05-17 17:14:23 +0200

    first commit

---
+++ projects/angular-google-tag-manager/src/lib/angular-google-tag-manager.service.ts
@@ -0,0 +1,65 @@
+import {Inject, Injectable} from '@angular/core';
+import {BrowserGlobalsService} from 'browser-globals';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class GoogleTagManagerService {
+
+    private isLoaded = false;
+    private gtmId: string;
+
+    constructor(
+        private browserGlobals: BrowserGlobalsService,
+        @Inject('googleTagManagerId') public googleTagManagerId: string,
+    ) {
+        this.gtmId = googleTagManagerId;
+    }
+
+    public getDataLayer() {
+        const window = this.browserGlobals.windowRef();
+        window['dataLayer'] = window['dataLayer'] || [];
+        return window['dataLayer'];
+    }
+
+    private pushOnDataLayer(obj: object) {
+        const dataLayer = this.getDataLayer();
+        dataLayer.push(obj);
+    }
+
+    public addGtmToDom() {
+        const doc = this.browserGlobals.documentRef();
+        this.pushOnDataLayer({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+        });
+
+        const gtmScript = doc.createElement('script');
+        gtmScript.id = 'GTMscript';
+        gtmScript.async = true;
+        gtmScript.src = '//www.googletagmanager.com/gtm.js?id=' + this.gtmId;
+        doc.head.insertBefore(gtmScript, doc.head.firstChild);
+
+        const ifrm = doc.createElement('iframe');
+        ifrm.setAttribute('src', '//www.googletagmanager.com/ns.html?id=' + this.gtmId);
+        ifrm.style.width = '0';
+        ifrm.style.height = '0';
+        ifrm.style.display = 'none';
+        ifrm.style.visibility = 'hidden';
+
+        const noscript = doc.createElement('noscript');
+        noscript.id = 'GTMiframe';
+        noscript.appendChild(ifrm);
+
+        doc.body.insertBefore(noscript, doc.body.firstChild);
+        this.isLoaded = true;
+    }
+
+    public pushTag(item: object) {
+        if (!this.isLoaded) {
+            this.addGtmToDom();
+        }
+        this.pushOnDataLayer(item);
+    }
+
+}
---
+++ projects/angular-google-tag-manager/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of angular-google-tag-manager
+ */
+
+export * from './lib/angular-google-tag-manager.service';
+
